2ef76c4bfef0404419a27bf049f876ff08760efa
Carlos Fuentes <cafuentes1@uc.cl>
2018-04-17 18:06:42 -0300

    Agregado Registrar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   validates :nombre, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with:VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with:VALID_EMAIL_REGEX },
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  match '/signup', to: 'users#new', via: 'get'
+
   root 'welcome#index'
 end
