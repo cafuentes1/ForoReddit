e4fd98a1e57f6a5a23a1fdd70470bc17535353c3
Carlos Fuentes <cafuentes1@uc.cl>
2018-06-08 11:58:44 -0400

    User Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,28 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
+
   root 'forums#index'
 
-  devise_for :users, controllers: { registrations: 'registrations'}
+  #devise_for :users, controllers: { registrations: 'registrations'}
+
+  devise_for :users, skip: [:sessions, :registrations]
+
+  devise_scope :user do
+    # sessions
+    get    'login',  to: 'devise/sessions#new',     as: :new_user_session
+    post   'login',  to: 'devise/sessions#create',  as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    # registrations
+    put    '/account',  to: 'devise/registrations#update'
+    delete '/account',  to: 'devise/registrations#destroy'
+    post   '/account',  to: 'devise/registrations#create'
+    get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
+    get    '/account',  to: 'devise/registrations#edit',   as: :edit_user_registration
+    patch  '/account',  to: 'devise/registrations#update', as: :user_registration
+    get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration# passwords
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
