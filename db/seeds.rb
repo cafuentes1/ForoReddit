e3aef6c758c212cf9c2c970432b6be8dd7908951
Carlos Fuentes <cafuentes1@uc.cl>
2018-06-28 18:30:50 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,3 @@
-ReputationSystem::Reputation.create!([
-  {reputation_name: "votes", value: -1.0, aggregated_by: "sum", target_type: "Comment", target_id: 17, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Post", target_id: 6, active: true, data: {}},
-  {reputation_name: "posts_votes", value: 4.0, aggregated_by: "sum", target_type: "User", target_id: 2, active: true, data: {}},
-  {reputation_name: "rp", value: 6.0, aggregated_by: "sum", target_type: "User", target_id: 2, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "Comment", target_id: 25, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "Post", target_id: 11, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Post", target_id: 13, active: true, data: {}},
-  {reputation_name: "posts_votes", value: 4.0, aggregated_by: "sum", target_type: "User", target_id: 1, active: true, data: {}},
-  {reputation_name: "rp", value: 5.0, aggregated_by: "sum", target_type: "User", target_id: 1, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Post", target_id: 2, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Comment", target_id: 19, active: true, data: {}},
-  {reputation_name: "votes", value: -1.0, aggregated_by: "sum", target_type: "Comment", target_id: 21, active: true, data: {}},
-  {reputation_name: "votes", value: -1.0, aggregated_by: "sum", target_type: "Comment", target_id: 22, active: true, data: {}},
-  {reputation_name: "votes", value: 5.0, aggregated_by: "sum", target_type: "User", target_id: 1, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Post", target_id: 10, active: true, data: {}},
-  {reputation_name: "votes", value: 2.0, aggregated_by: "sum", target_type: "Post", target_id: 8, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "Comment", target_id: 14, active: true, data: {}},
-  {reputation_name: "votes", value: -1.0, aggregated_by: "sum", target_type: "Post", target_id: 1, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Comment", target_id: 20, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Post", target_id: 3, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Comment", target_id: 15, active: true, data: {}},
-  {reputation_name: "votes", value: 2.0, aggregated_by: "sum", target_type: "Post", target_id: 9, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "User", target_id: 2, active: true, data: {}},
-  {reputation_name: "votes", value: -2.0, aggregated_by: "sum", target_type: "Post", target_id: 5, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "Comment", target_id: 24, active: true, data: {}},
-  {reputation_name: "votes", value: 1.0, aggregated_by: "sum", target_type: "Comment", target_id: 16, active: true, data: {}},
-  {reputation_name: "votes", value: 0.0, aggregated_by: "sum", target_type: "Comment", target_id: 23, active: true, data: {}},
-  {reputation_name: "comments_votes", value: 0.0, aggregated_by: "sum", target_type: "User", target_id: 2, active: true, data: {}}
-])
-ReputationSystem::ReputationMessage.create!([
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 1, receiver_id: 1, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 2, receiver_id: 2, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 3, receiver_id: 3, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 4, receiver_id: 4, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 7, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 5, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 4, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 3, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 1, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 2, receiver_id: 8, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 5, receiver_id: 5, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 6, receiver_id: 2, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 7, receiver_id: 1, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 9, receiver_id: 11, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 7, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 5, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 4, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 3, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 1, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 2, receiver_id: 12, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 6, receiver_id: 13, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 10, receiver_id: 6, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 6, receiver_id: 14, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 14, receiver_id: 15, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 11, receiver_id: 16, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 12, receiver_id: 17, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 12, receiver_id: 19, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 13, receiver_id: 7, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 15, receiver_id: 21, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 21, receiver_id: 18, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 16, receiver_id: 23, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 23, receiver_id: 18, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 17, receiver_id: 24, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 24, receiver_id: 18, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 18, receiver_id: 25, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 25, receiver_id: 18, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 19, receiver_id: 22, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 22, receiver_id: 14, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 20, receiver_id: 26, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 26, receiver_id: 14, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 21, receiver_id: 27, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 27, receiver_id: 18, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 22, receiver_id: 4, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 23, receiver_id: 26, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 24, receiver_id: 27, weight: 1.0},
-  {sender_type: "ReputationSystem::Evaluation", sender_id: 25, receiver_id: 30, weight: 1.0},
-  {sender_type: "ReputationSystem::Reputation", sender_id: 30, receiver_id: 12, weight: 1.0}
-])
 User::HABTM_Roles.create!([
   {user_id: 1, role_id: 1}
 ])
@@ -117,11 +38,6 @@ Comment.create!([
   {comment: "hola", post_id: 5, user_id: 2},
   {comment: "hi", post_id: 11, user_id: 1}
 ])
-FavoritePost.create!([
-  {post_id: 11, user_id: 1},
-  {post_id: 13, user_id: 1},
-  {post_id: 13, user_id: 2}
-])
 Forum.create!([
   {title: "General", topic: "Test", description: "Test Forum", post_id: nil},
   {title: "Blade Runner", topic: "Movies", description: "This is a forum about Blade Runner", post_id: nil},
@@ -133,9 +49,4 @@ Forum.create!([
 ])
 Role.create!([
   {name: "admin", resource_type: nil, resource_id: nil}
-])
-SubscriptionForum.create!([
-  {forum_id: 1, user_id: 1},
-  {forum_id: 2, user_id: 1},
-  {forum_id: 3, user_id: 1}
 ])
