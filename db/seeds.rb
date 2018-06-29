1955f375683311aa43b446e3b895297662373871
Carlos Fuentes <cafuentes1@uc.cl>
2018-06-28 21:38:05 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,28 @@
+
 User.create!([
-  {email: "cafuentes1@uc.cl", encrypted_password: "$2a$11$ltmjOGDoW3QNfcUWDx.yuOL20hsAuWKi1wzixCuhxr.Kyjk67U1WG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2018-06-25 03:49:05", last_sign_in_at: "2018-06-24 02:26:01", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", username: "cafuentes1"},
-  {email: "cafuentes2@uc.cl", encrypted_password: "$2a$11$G/Z/ZMfkF8IXIpMCEzvTj.NquxhoIPTy2tGSUgCEFF7IHrVyGQCSC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 12, current_sign_in_at: "2018-06-25 06:04:08", last_sign_in_at: "2018-06-25 03:16:05", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", username: "cafuentes2"}
+  {email: "cafuentes1@uc.cl", password: 123456, :password_confirmation: 123456, username: "cafuentes1"},
+  {email: "cafuentes2@uc.cl", password: 123456, :password_confirmation: 123456, username: "cafuentes2"}
+])
+Forum.create!([
+  {title: "General", topic: "Test", description: "Test Forum"},
+  {title: "Blade Runner", topic: "Movies", description: "This is a forum about Blade Runner"},
+  {title: "Star Wars", topic: "Movies", description: "This is a forum about Star Wars precuels"},
+  {title: "Spider-man", topic: "Movies", description: "This is a forum about The Holy Trilogy."},
+  {title: "Mad Men", topic: "Series", description: "This is a forum about Mad Men TV series."},
+  {title: "The Beatles", topic: "Music", description: "Forum about The Beatles"},
+  {title: "Queen", topic: "Music", description: "Forum about the band Queen"}
 ])
 Post.create!([
-  {title: "Test Post", content: "This is a test post used for testing our forum. Edited. Edited.", user_id: 1, forum_id: 1},
-  {title: "Test Post 2", content: "Este es el segundo post de testeo de la pagina.", user_id: 2, forum_id: 3},
-  {title: "Tercer Post", content: "Este es el tercer post de la pagina. ", user_id: 1, forum_id: 2},
-  {title: "Post numero 4", content: "Este es otro post", user_id: 1, forum_id: 4},
-  {title: "Test 5 del foro", content: "Este es un post de testeo", user_id: 1, forum_id: 1},
-  {title: "Post con imagen", content: "Post imagen", user_id: 1, forum_id: 1},
-  {title: "Post 30", content: "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)", user_id: 1, forum_id: 2},
-  {title: "Testeo de Up/Down", content: "Este post se usara para testear funcionalidad Up/Down", user_id: 2, forum_id: 4},
-  {title: "Paul", content: "Let's talk about McCartney", user_id: 2, forum_id: 6},
-  {title: "Bohemian Rhapsody", content: "Good song.\r\n\r\n![Image of Queen](http://e.radio-oxigeno.io/normal/2018/05/24/292129_615616.jpg)", user_id: 1, forum_id: 7}
+  {title: "Test Post", content: "This is a test post used for testing our forum. Edited. Edited.", user_id: 1},
+  {title: "Test Post 2", content: "Este es el segundo post de testeo de la pagina.", user_id: 2},
+  {title: "Tercer Post", content: "Este es el tercer post de la pagina. ", user_id: 1},
+  {title: "Post numero 4", content: "Este es otro post", user_id: 1},
+  {title: "Test 5 del foro", content: "Este es un post de testeo", user_id: 1},
+  {title: "Post con imagen", content: "Post imagen", user_id: 1},
+  {title: "Post 30", content: "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)", user_id: 1},
+  {title: "Testeo de Up/Down", content: "Este post se usara para testear funcionalidad Up/Down", user_id: 2},
+  {title: "Paul", content: "Let's talk about McCartney", user_id: 2},
+  {title: "Bohemian Rhapsody", content: "Good song.\r\n\r\n![Image of Queen](http://e.radio-oxigeno.io/normal/2018/05/24/292129_615616.jpg)", user_id: 1}
 ])
 Comment.create!([
   {comment: "Comentario editado", post_id: 1, user_id: 1},
@@ -32,12 +42,12 @@ Comment.create!([
   {comment: "hola", post_id: 5, user_id: 2},
   {comment: "hi", post_id: 11, user_id: 1}
 ])
-Forum.create!([
-  {title: "General", topic: "Test", description: "Test Forum", post_id: nil},
-  {title: "Blade Runner", topic: "Movies", description: "This is a forum about Blade Runner", post_id: nil},
-  {title: "Star Wars", topic: "Movies", description: "This is a forum about Star Wars precuels", post_id: nil},
-  {title: "Spider-man", topic: "Movies", description: "This is a forum about The Holy Trilogy.", post_id: nil},
-  {title: "Mad Men", topic: "Series", description: "This is a forum about Mad Men TV series.", post_id: nil},
-  {title: "The Beatles", topic: "Music", description: "Forum about The Beatles", post_id: nil},
-  {title: "Queen", topic: "Music", description: "Forum about the band Queen", post_id: nil}
+Role.create!([
+  {name: "admin", resource_type: nil, resource_id: nil}
+])
+User::HABTM_Roles.create!([
+  {user_id: 1, role_id: 1}
+])
+Role::HABTM_Users.create!([
+  {user_id: 1, role_id: 1}
 ])
